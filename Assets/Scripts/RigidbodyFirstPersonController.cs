07ffe3e5afcffc6824667f48aa2d834c953b10d9
Naomi Wiggins <Naomi.Wig8464@mds.network>
2020-07-30 11:48:01 +1200

    update

--- Assets/Scripts/RigidbodyFirstPersonController.cs
+++ Assets/Scripts/RigidbodyFirstPersonController.cs
@@ -12,14 +12,14 @@ namespace UnityStandardAssets.Characters.FirstPerson
         [Serializable]
         public class MovementSettings
         {
-            public float ForwardSpeed = 5.0f;   // Speed when walking forward
-            public float BackwardSpeed = 5.0f;  // Speed when walking backwards
-            public float StrafeSpeed = 5.0f;    // Speed when walking sideways
+            public float ForwardSpeed = 4.0f;   // Speed when walking forward
+            public float BackwardSpeed = 4.0f;  // Speed when walking backwards
+            public float StrafeSpeed = 4.0f;    // Speed when walking sideways
             public float RunMultiplier = 2.0f;   // Speed when sprinting
 	        public KeyCode RunKey = KeyCode.LeftShift;
-            public float JumpForce = 7f;     //30f - default
+            public float JumpForce = 5f;     //30f - default
             public float jumpForceHangY = 5f;
-            public float jumpForceHangX = 15f;
+            public float jumpForceHangX = 10f;
             public AnimationCurve SlopeCurveModifier = new AnimationCurve(new Keyframe(-90.0f, 1.0f), new Keyframe(0.0f, 1.0f), new Keyframe(90.0f, 0.0f));
             [HideInInspector] public float CurrentTargetSpeed = 8f;
 
