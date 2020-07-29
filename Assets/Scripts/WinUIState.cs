fccd5a524760d408a5996affba22cca7f2e61cc5
Matthew Lee <Matthew.Lee8637@mds.network>
2020-07-29 18:26:50 +1200

    Experimented with some timers (commented out)
    tried a bit at doing timers. Not complete so I commented out. Not important atm

--- Assets/Scripts/WinUIState.cs
+++ Assets/Scripts/WinUIState.cs
@@ -1,13 +1,19 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class WinUIState : UIState
 {
-
+    //public Text WinText;
+    //public float Timer = 0.0f;
     void Start()
     {
-        
+    }
+
+    private void Update()
+    {
+        //WinText.text = "You Win! \n Press enter to restart";// \n Time: " + Timer; ;
     }
 
     private void FixedUpdate()
@@ -17,4 +23,9 @@ public class WinUIState : UIState
             Application.LoadLevel(Application.loadedLevel);
         }
     }
+
+    //public void RecordTime()
+    //{
+    //    Timer = Time.deltaTime;
+    //}
 }
